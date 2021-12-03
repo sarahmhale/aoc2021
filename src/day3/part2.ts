79c6e0157232a26a2a2778c0193ccbb361468be1
Sarah <sarah.hale@sellpy.se>
2021-12-03 12:39:20 +0100

    little bit of cleanup

--- src/day3/part2.ts
+++ src/day3/part2.ts
@@ -1,47 +1,31 @@
 import { parseInput } from "../utils/parseInput"
 
 let INDEX = 0
-let NR_OF_ONES = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 const removeMismatches = (arr: string[], val: string): string[] => arr.filter((binary) => binary[INDEX] === val)
+const nrOfBin = (input: string[], bin: string): number => input.filter((curr: string) => curr[INDEX] === bin).length
 
 
-const getNrOfOnes = (curr: string): void => {
-  if (curr.split('')[INDEX] === '1')
-    NR_OF_ONES[INDEX] = NR_OF_ONES[INDEX] + 1
-}
+const oxygenGeneratorRating = (input: string[]) => (nrOfBin(input, '1') < (input.length / 2)) ? removeMismatches(input, '0') : removeMismatches(input, '1')
+const co2ScrubberRating = (input: string[]) => (nrOfBin(input, '0') > (input.length / 2)) ? removeMismatches(input, '1') : removeMismatches(input, '0')
 
 
-const working = (input: string[]) => {
-  input.map((curr: string) => getNrOfOnes(curr))
-  if (NR_OF_ONES[INDEX] >= (input.length / 2)) return removeMismatches(input, '1')
-  else return removeMismatches(input, '0')
-}
+export const run = (): number => {
+  let input: any = parseInput({ split: { delimiter: '\n', mapper: false } })
 
-const workingEpsilon = (input: string[]) => {
-  input.map((curr: string) => getNrOfOnes(curr))
-  if (NR_OF_ONES[INDEX] < (input.length / 2)) return removeMismatches(input, '1')
-  else return removeMismatches(input, '0')
-}
-
-export const run = () => {
-  let gamma: any = parseInput({ split: { delimiter: '\n', mapper: false } })
-  let epsilon: any = parseInput({ split: { delimiter: '\n', mapper: false } })
-
+  let gamma = input
   while (gamma.length > 1) {
-    NR_OF_ONES = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    gamma = working(gamma)
+    gamma = oxygenGeneratorRating(gamma)
     INDEX++
   }
 
   INDEX = 0
 
+  let epsilon = input
   while (epsilon.length > 1) {
-    NR_OF_ONES = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    epsilon = workingEpsilon(epsilon)
+    epsilon = co2ScrubberRating(epsilon)
     INDEX++
   }
 
   return parseInt(gamma, 2) * parseInt(epsilon, 2)
-
 }
